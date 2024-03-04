ec9f8d17e61f9b92d276f866f1dd6749dd6c3d0c
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-03-04 17:15:06 +0530

    done

--- script.js
+++ script.js
@@ -21,22 +21,65 @@ fetch("https://restcountries.com/v3.1/all")
     });
 })
 
+let countrySelect = document.getElementById("country");
+
+fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+        let options = data.map((element) => element.name.common);
+
+        // Sort the options alphabetically
+        options.sort();
+
+        // Add the sorted options to the dropdown
+        options.forEach((optionText) => {
+            let option = document.createElement("option");
+            option.value = optionText;
+            option.text = optionText;
+            countrySelect.appendChild(option);
+        });
+    });
+
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+        let options = data.map((element) => element.name.common);
+
+        // Sort the options alphabetically
+        options.sort();
+
+        // Add the sorted options to the dropdown
+        options.forEach((optionText) => {
+            let option = document.createElement("option");
+            option.value = optionText;
+            option.text = optionText;
+            countrySelect.appendChild(option);
+        });
+    });
+
+
 const CName = document.getElementById("CName");
 const CCity = document.getElementById("CCity");
 const Currency = document.getElementById("Currency");
 const Region = document.getElementById("Region");
 const Languages = document.getElementById("Languages");
-
+const UserCountry = document.getElementById("countryName");
+const countrySelect1 = document.getElementById("country");
 const flag = document.getElementById("flag");
 const Location = document.getElementById("Location");
 
 
 
-function countryCheck(){
-    const InputCountry = document.getElementById("countryName").value.toLowerCase();
-    if(countryList.includes(InputCountry)){
+
+function countryCheck22(){
+    console.log(UserCountry.value.length);
+   console.log(countrySelect1.value.length);
+
+   if(UserCountry.value.length >= 0 &&  countrySelect1.value == "Select A country" ){
+    if(countryList.includes(UserCountry.value.toLowerCase())){
         for (let i = 0; i < countryList.length; i++) {
-            if (countryList[i] === InputCountry) {
+            if (countryList[i] === UserCountry.value.toLowerCase()) {
                 index = i;
                 break; // Once found, break the loop
             }
@@ -57,6 +100,11 @@ function countryCheck(){
                 Languages.textContent = Object.values(country.languages).join(", ");
                 flag.src = country.flags.png;
                 Location.href = country.maps.googleMaps;
+
+
+
+
+                
             }
         })
         .catch((error) => {
@@ -67,6 +115,57 @@ function countryCheck(){
     }
 
 
+    
+   }else if(UserCountry.value.length <= 0 &&  countrySelect1.value != "Select A country"){
+    if(countryList.includes(countrySelect1.value.toLowerCase())){
+        for (let i = 0; i < countryList.length; i++) {
+            if (countryList[i] === countrySelect1.value.toLowerCase()) {
+                index = i;
+                break; // Once found, break the loop
+            }
+        }
+        const countryName = countryNameNew[index]; // Replace "Your Country Name" with the name of the country you want to retrieve details for
+    
+    fetch("https://restcountries.com/v3.1/all")
+        .then((res) => res.json())
+        .then((data) => {
+            const country = data.find((element) => element.name.common === countryName);
+    
+            if (country) {
+                console.log(country);
+                CName.textContent = country.name.common;
+                CCity.textContent = country.capital;
+                Currency.textContent = country.currencies[Object.keys(country.currencies)[0]].name;
+                Region.textContent = country.region;
+                Languages.textContent = Object.values(country.languages).join(", ");
+                flag.src = country.flags.png;
+                Location.href = country.maps.googleMaps;
+
+
+
+
+                
+            }
+        })
+        .catch((error) => {
+            console.log("Error:", error);
+        });
+    }else{
+        alert("Country Not Found");
+    }
+
+
+   }else{
+      alert("Please Use one method")
+   }
+
+}
+
+
+
+function countryCheck(){
+    
+   
     
    
 }
