18d29d67af83ac74327363a02b2b5df4cb9c04e5
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-03-04 13:26:41 +0530

    done

--- script.js
+++ script.js
@@ -12,26 +12,57 @@ fetch("https://restcountries.com/v3.1/all")
     });
 })
 
-
 fetch("https://restcountries.com/v3.1/all")
 .then((res) => res.json())
 .then((data) => {
     data.forEach(element => {
         const countryName = (element.name.common);
-        countryList.push(countryNameNew);
+        countryNameNew.push(countryName);
     });
 })
+
+const CName = document.getElementById("CName");
+const CCity = document.getElementById("CCity");
+const Currency = document.getElementById("Currency");
+const Region = document.getElementById("Region");
+const Languages = document.getElementById("Languages");
+
 
 
 function countryCheck(){
     const InputCountry = document.getElementById("countryName").value.toLowerCase();
-    
     if(countryList.includes(InputCountry)){
-        alert("Country Found");
+        for (let i = 0; i < countryList.length; i++) {
+            if (countryList[i] === InputCountry) {
+                index = i;
+                break; // Once found, break the loop
+            }
+        }
+        const countryName = countryNameNew[index]; // Replace "Your Country Name" with the name of the country you want to retrieve details for
+    
+    fetch("https://restcountries.com/v3.1/all")
+        .then((res) => res.json())
+        .then((data) => {
+            const country = data.find((element) => element.name.common === countryName);
+    
+            if (country) {
+                CName.textContent = country.name.common;
+                CCity.textContent = country.capital;
+                Currency.textContent = country.currencies[Object.keys(country.currencies)[0]].name;
+                Region.textContent = country.region;
+                Languages.textContent = Object.values(country.languages).join(", ");
+            }
+        })
+        .catch((error) => {
+            console.log("Error:", error);
+        });
     }else{
         alert("Country Not Found");
     }
+
+
     
+   
 }
 
 ;
